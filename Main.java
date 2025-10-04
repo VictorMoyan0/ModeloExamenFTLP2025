e28ab94b5d82153db4732b6d747500fb77ba2ef4
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-10-04 15:38:08 -0300

    agregando constructor

--- Main.java
+++ Main.java
@@ -7,5 +7,15 @@ public abstract class Vehiculo {
     private String id;
     private double kilometraje;
     private double capacidadCarga;
-
+    // Constructor
+    public Vehiculo(String id, double kilometraje, double capacidadCarga) {
+        this.id = id;
+        this.kilometraje = kilometraje;
+        this.capacidadCarga = capacidadCarga;
+    }
+    // Getters y Setters
+    public String getId() { return id; }
+    public double getKilometraje() { return kilometraje; }
+    public void setKilometraje(double km) { this.kilometraje = km; }
+    public double getCapacidadCarga() { return capacidadCarga; }
 }
