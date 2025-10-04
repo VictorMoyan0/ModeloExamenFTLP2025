6884aa4e912e7afeedfa42c0c163763f94239d68
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-10-04 15:51:31 -0300

    creando metodos clase furgoneta

--- Main.java
+++ Main.java
@@ -78,4 +78,29 @@ public class Furgoneta extends Vehiculo {
         this.volumen = volumen;
         this.tarifaBase = tarifaBase;
     }
+    public double calcularCostoMantenimiento() {
+        if (getKilometraje() > 50000) {
+            return tarifaBase * 1.2; // recargo por km
+        }
+        return tarifaBase;
+    }
+    public String simularJornada(double kmRecorridos) {
+        setKilometraje(getKilometraje() + kmRecorridos);
+        return "Furgoneta " + getId() + " recorrió " + kmRecorridos + " km, total: " + getKilometraje();
+    }
+    public String obtenerEtiquetaAmbiental() {
+        return "Amarilla";
+    }
+    public int evaluarRiesgo() {
+        return getKilometraje() > 75000 ? 7 : 3;
+    }
+    public String diagnosticarNeumaticos() {
+        return "Revisar desgaste y alineación";
+    }
+    public boolean chequeoFrenado() {
+        return getKilometraje() > 75000;
+    }
+    public double calcularTiempoCarga(double cantidadKg) {
+        return cantidadKg / 1500.0; // velocidad media de carga 1500 kg/h
+    }
 }
