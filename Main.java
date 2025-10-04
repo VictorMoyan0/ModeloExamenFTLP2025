5434ccfb6d905c10b409803529dfbba13b1c35f2
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-10-04 11:30:21 -0300

    caracteristicas comunes

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+
+}
+
+public abstract class Vehiculo {
+    // Propiedades comunes
+    private String id;
+    private double kilometraje;
+    private double capacidadCarga;
+
 }
