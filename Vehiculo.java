aef889357b1b7f301967296fe14acf27556fcb48
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-10-06 20:36:56 -0300

    estructura inicial

---
+++ Vehiculo.java
@@ -0,0 +1,12 @@
+public class Vehiculo {
+    public static void main(double CostoMantenimiento, String SimulacionJornada, String EtiquetaAmbiental, int EvalucionRiesgo, String DiagnosticoNeumaticos, boolean ChequeoFrenado, double TiempoCarga) {
+        // Variables
+        double costoMantenimiento = CostoMantenimiento;
+        String simulacionJornada = SimulacionJornada;
+        String etiquetaAmbiental = EtiquetaAmbiental;
+        int evalucionRiesgo = EvalucionRiesgo;
+        String diagnosticoNeumaticos = DiagnosticoNeumaticos;
+        boolean chequeoFrenado = ChequeoFrenado;
+        double tiempoCarga = TiempoCarga;
+    }
+}
